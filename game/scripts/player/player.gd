5bca0bcf3d0b6c7072135ae8f8ea6592f74540fb
Michael Wiest <michaelwiest>
2023-03-25 16:41:49 -0700

    more sane movement

--- game/scripts/player/player.gd
+++ game/scripts/player/player.gd
@@ -7,18 +7,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var direction: Vector2 = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
-	if direction:
+	if direction.y:
 		var velocity_y: float = velocity.y + sign(direction.y) * acceleration
 		if velocity.y < 0:
 			velocity.y = max(velocity_y, -max_speed * abs(direction.y))
 		else:
 			velocity.y = min(velocity_y, max_speed * abs(direction.y))
+	else:
+		
+		velocity.y = move_toward(velocity.y, 0, friction)
+	if direction.x:
 		var velocity_x: float = velocity.x + sign(direction.x) * acceleration
 		if velocity.x < 0:
 			velocity.x = max(velocity_x, -max_speed * abs(direction.x))
 		else:
 			velocity.x = min(velocity_x, max_speed * abs(direction.x))
-	else:
+	else: 
 		velocity.x = move_toward(velocity.x, 0, friction)
-		velocity.y = move_toward(velocity.y, 0, friction)
+			
 	move_and_slide()
